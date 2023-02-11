8ced07b28c41ee10727f09794779f6f6a6db47e4
Daniel <dgabrov@cbox.info>
2023-02-10 23:58:42 -0500

    comments

--- src/Li.tsx
+++ src/Li.tsx
@@ -13,6 +13,14 @@ const Li = (props: { id: string, nameHere: string, clb: (current: string, over: 
         })
     }))
 
+    // please see drop: it is important, it is the function that is being called when drop happens
+    // this function is invoked with the object being dropped and the monitor that shows the state of
+    // the drop operation. The first object is the object being dragged, defined as itm inside the useDrag call
+
+    // VERY IMPORTANT - you cannot make the same html tag to be a drag source and a drop target, however
+    // I put the li html tag inside a drag, I made the drag source the li html tag and the drop target
+    // the encompassing div tag
+
     const [{ isOver}, drop] = useDrop(() => ({
         accept: "whatever",
         drop: (alfa: ItemData, monitor) => {
